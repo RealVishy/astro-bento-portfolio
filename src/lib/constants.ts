fb41a145f720eaf89f3918bd9ecca499bd5b78b1
Vishwas P <62875653+RealVishy@users.noreply.github.com>
2024-12-15 17:12:51 +1100

    add new linkedin url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: "https://github.com/RealVishy",
-  linkedin: "https://www.linkedin.com/in/vishwas-parpattegar-3b027a228/",
+  linkedin: "https://www.linkedin.com/in/vishwasparpattegar",
   medium: "https://ladvace.medium.com/",
   trakt: "https://trakt.tv/users/realdishwash",
   email: "vishwasparpattegar@gmail.com",
